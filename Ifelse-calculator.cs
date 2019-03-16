d8cf2bba486b37739640a447bf248f0541dc341f
fakhriikhwll <fakhrian.im@gmail.com>
2019-03-16 17:10:08 +0700

    inisialisasi project

---
+++ Ifelse-calculator.cs
@@ -0,0 +1,87 @@
+using System;
+
+namespace CaclulatorConsoleApp
+{
+    class Program
+	/* Nama : Muhammad Fakhrian Ikhwaludin
+	   Nim  : 18 - 11 - 2265
+	   Kelas: Informatika-7 */
+    {
+        static void Main(string[] args)
+        {
+            
+            //Opsi kalkulator
+            Console.WriteLine("Pilihan operasi hitung kalkulator");
+            Console.WriteLine("_________________________________");
+            Console.WriteLine("1. Perkalian");
+            Console.WriteLine("2. Penjumlahan");
+            Console.WriteLine("3. Pengurangan");
+            Console.WriteLine("4. Pembagian");
+            Console.WriteLine("__________________________________");
+
+            Console.Write("Masukan pilihan perhitungan : ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+
+            //Percabangan & eksekusi nilai
+            if (choice == 1)
+            {
+                //Input nilai
+                Console.Write("\n\nMasukan nilai a : ");
+                float a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nMasukan nilai b : ");
+                float b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\nHasil " + a + " * " + b + " = " + (a * b));
+                Console.WriteLine();
+                Console.WriteLine("\n\nTekan sembarang tombol untuk keluar");
+                Console.Read();
+            }
+            else if (choice == 2)
+            {
+                //Input nilai
+                Console.Write("\n\nMasukan nilai a : ");
+                float a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nMasukan nilai b : ");
+                float b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\nHasil " + a + " + " + b + " = " + (a + b));
+                Console.WriteLine();
+                Console.WriteLine("\n\nTekan sembarang tombol untuk keluar");
+                Console.Read();
+            }
+            else if (choice == 3)
+            {
+                //Input nilai
+                Console.Write("\n\nMasukan nilai a : ");
+                float a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nMasukan nilai b : ");
+                float b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\nHasil " + a + " - " + b + " = " + (a - b));
+                Console.WriteLine();
+                Console.WriteLine("\n\nTekan sembarang tombol untuk keluar");
+                Console.Read();
+            }
+            else if (choice == 4)
+            {
+                //Input nilai
+                Console.Write("\n\nMasukan nilai a : ");
+                float a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("\nMasukan nilai b : ");
+                float b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n\nHasil " + a + " / " + b + " = " + (a / b));
+                Console.WriteLine();
+                Console.WriteLine("\n\nTekan sembarang tombol untuk keluar");
+                Console.Read();
+            }
+
+            else
+            {
+
+                Console.WriteLine("\n\nPastikan memilih antara menu 1-4");
+                Console.WriteLine("\n\nTekan sembarang tombol untuk keluar");
+                Console.Read();
+            }
+
+            
+        }
+    }
+}
